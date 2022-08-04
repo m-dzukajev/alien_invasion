518f1f3789b8daaf842fc9d15d03b49f90153f5b
Marat <roulinwhite@gmail.com>
2022-08-04 17:14:22 +0300

    Add Settings class, Ship class, image.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,25 @@
 import sys
 import pygame
-
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
-    """Класс для управления ресурсами и поведением игры. """
+    """Класс для управления ресурсами и поведением игры."""
 
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
+        self.settings = Settings()  # настройки для игры, импортируем с файла settings
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               self.settings.screen_height))
+
         pygame.display.set_caption("Alien Invasion")
+        # Назначение цвета фона.
+        self.bg_color = self.settings.bg_color
+
+        #Инициализация корабля игрока
+        self.ship = Ship(self)
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -19,8 +28,11 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-        # Отображение последнего прорисованного экрана.
-        pygame.display.flip()
+            # При каждом проходе цикла перерисовывается экран.
+            self.screen.fill(self.bg_color)
+            self.ship.blitme()
+            # Отображение последнего прорисованного экрана.
+            pygame.display.flip()
 
 
 if __name__ == '__main__':

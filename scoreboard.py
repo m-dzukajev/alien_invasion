ffc146ac0e89afb1de2396a75e1145a6663ec930
Marat <roulinwhite@gmail.com>
2022-08-11 15:27:48 +0300

    Add max points, add collision check, for better points count

--- scoreboard.py
+++ scoreboard.py
@@ -16,6 +16,8 @@ class Scoreboard:
         self.font = pygame.font.SysFont(None, 48)
         # Подготовка исходного изображения.
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Преобразует текущий счет в графическое изображение."""
@@ -28,6 +30,37 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Преобразует рекордный счет в графическое изображение."""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True,
+                                                 self.text_color, self.settings.bg_color)
+
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def check_high_score(self):
+        """Проверяет, появился ли новый рекорд."""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
+
     def show_score(self):
         """Выводит счет на экран."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+
+    def prep_level(self):
+        """Преобразует уровень в графическое изображение."""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True,
+                                            self.text_color, self.settings.bg_color)
+
+        # Уровень выводится под текущим счетом.
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
